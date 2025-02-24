6cfe009dc663a8aa55849c2aaebac6253c2df433
sre <63744863+sr3y@users.noreply.github.com>
2025-02-24 02:43:14 -0500

    Add code files

---
+++ test.py
@@ -0,0 +1,11 @@
+# test_app.py
+
+import pytest
+from main import app
+
+
+def test_health_check():
+    client = app.test_client()
+    response = client.get('/health')
+    assert response.status_code == 200
+    assert response.json['status'] == 'success'
